f6c6aab69be46eaaa35ffd4bfedef8c928a166d2
Denis Alves <dee.alves2@gmail.com>
2021-07-02 22:13:56 -0300

    utilizando forEach lambda com index e mascara de formatação

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,11 @@
 fun main() {
-    val nomes = arrayOf("Marcos", "Dênis", "Junior")
-    
-    nomes.forEach {
-        print("$it ")
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 2000.0
+    salarios[2] = 3000.0
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
     }
+    salarios.forEach { println("%.2f".format(it)) }
 }
