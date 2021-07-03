352c919668765bc3d85a4f0c795e2141ed5320fb
Denis Alves <dee.alves2@gmail.com>
2021-07-02 23:30:22 -0300

    trabalhando com quantidade de itens em um array

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,5 +10,5 @@ fun main() {
     val salariosMaiorQue2500 = salarios.filter { it > 2500 }
     salariosMaiorQue2500.forEach({ println("Salário filtrado: $it") })
 
-    
+    println("Quantidade de salários entre 2000 e 5000: ${salarios.count {it in 2000.0..5000.0}}")
 }
