69c721cbb08989fea64be4f9dd9b2a802685733e
Denis Alves <dee.alves2@gmail.com>
2021-07-02 22:16:02 -0300

    utilizando doubleArrayOf

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,5 @@
 fun main() {
-    val salarios = DoubleArray(3)
-    salarios[0] = 1000.0
-    salarios[1] = 2000.0
-    salarios[2] = 3000.0
-
-    salarios.forEachIndexed { index, salario ->
-        salarios[index] = salario * 1.1
-    }
-    salarios.forEach { println("%.2f".format(it)) }
+    val salarios = doubleArrayOf(1500.0, 1250.0, 5000.0)
+    salarios.sort()
+    salarios.forEach { println(it) }
 }
