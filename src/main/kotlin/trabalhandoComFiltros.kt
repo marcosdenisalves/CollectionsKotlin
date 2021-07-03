9f1d9f78b4fb3765332ded5dd38c62516ed5904b
Denis Alves <dee.alves2@gmail.com>
2021-07-02 23:42:17 -0300

    Trabalhando com filtros

--- src/main/kotlin/trabalhandoComFiltros.kt
+++ src/main/kotlin/trabalhandoComFiltros.kt
@@ -4,11 +4,11 @@ fun main() {
     println("Maior salário: ${salarios.maxOrNull()}")
     println("Menor salário: ${salarios.minOrNull()}")
     println("Média salarial: %.2f".format(salarios.average()))
-    println()
-
     println("========================")
     val salariosMaiorQue2500 = salarios.filter { it > 2500 }
     salariosMaiorQue2500.forEach({ println("Salário filtrado: $it") })
+    println("========================")
+    println("Encontrei o valor: ${salarios.find {it == 2250.0 }}") //busca por um valor que atenda a condição
 
-    println("Quantidade de salários entre 2000 e 5000: ${salarios.count {it in 2000.0..5000.0}}")
+    println("Valor passado foi encontrado? ${salarios.any { it == 1000.0 }}")
 }
