bff1e6522e518218c1c561aa872f23913b142fe8
Denis Alves <dee.alves2@gmail.com>
2021-07-03 00:11:19 -0300

    Trabalhando com agrupamentos

--- src/main/kotlin/trabalhandoComListasDeObjetos.kt
+++ src/main/kotlin/trabalhandoComListasDeObjetos.kt
@@ -1,29 +1,31 @@
 fun main() {
-    val joao = Funcionario(nome = "João", salario = 3500.0)
-    val maria = Funcionario(nome = "Maria", salario = 2500.0)
-    val eduarda = Funcionario(nome = "Eduarda", salario = 4000.0)
+    val joao = Funcionario(nome = "João", salario = 3500.0, tipoContrato = "PJ")
+    val maria = Funcionario(nome = "Maria", salario = 2500.0, tipoContrato = "CLT")
+    val eduarda = Funcionario(nome = "Eduarda", salario = 4000.0, tipoContrato = "PJ")
 
     val funcionarios = listOf(joao, maria, eduarda)
-    funcionarios.forEach({ println(it) })
-
-    println("================")
-    println(funcionarios.find { it.nome == "Eduarda" })
+//    funcionarios.forEach({ println(it) })
+//
+//    println("================")
+//    println(funcionarios.find { it.nome == "Eduarda" })
 
     println()
     println("================")
     funcionarios
-        .sortedBy { it.salario }
+        .groupBy { it.tipoContrato }
         .forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContrato: String
 ) {
     override fun toString(): String =
         """
             Nome: $nome
             Salário: $salario
+            Tipo de Contráto: $tipoContrato
             
         """.trimIndent()
 }
